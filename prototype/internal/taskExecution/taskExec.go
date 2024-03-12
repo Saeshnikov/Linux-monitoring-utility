3d1a9ce3744c0109e73a69407fa9c99a9932c306
mihyilo <90507951+mihyilo@users.noreply.github.com>
2024-03-12 23:11:04 +0300

    Update taskExec.go

--- prototype/internal/taskExecution/taskExec.go
+++ prototype/internal/taskExecution/taskExec.go
@@ -1,52 +1,31 @@
 package taskExecution
 
 import (
-	"fmt"
-	"os"
 	"sync"
 	"time"
 )
 
-func StartTasks(execTime int, fileName string, toRun func(int, string) *os.File, toAnalyse func(*os.File)) {
+func StartTasks(program_time int, bpftrace_time int, fileName string, toRun func(int, string)) {
 
 	var wg sync.WaitGroup
 
-	timer := time.After(time.Duration(execTime) * time.Second)
+	timer := time.After(time.Duration(program_time) * time.Second)
 
-	// канал для сигнала о завершении runBpftrace
-	done := make(chan *os.File)
-
-	// горутина для запуска bpftrace
 	bpftrace_run := func() {
 		defer wg.Done()
+		toRun(bpftrace_time, fileName)
+	}
 
-		for {
-			select {
-			case <-timer:
-				close(done)
-				fmt.Fprintln(os.Stdout, "Final!")
-				os.Exit(1)
-			default:
-				file := toRun(execTime, fileName)
-				done <- file
-			}
+	for {
+		select {
+		case <-timer:
+			wg.Wait()
+			return
+		default:
+			wg.Add(1)
+			go bpftrace_run()
+			time.Sleep(time.Duration(bpftrace_time)*time.Second - 2*time.Second)
 		}
 	}
 
-	// горутина для анализа вывода bpftrace
-	bpftrace_analyze := func() {
-		defer wg.Done()
-
-		for {
-			result := <-done
-			toAnalyse(result)
-			fmt.Fprintln(os.Stdout, "Written.")
-		}
-
-	}
-
-	wg.Add(2)
-	go bpftrace_run()
-	go bpftrace_analyze()
-	wg.Wait()
 }
