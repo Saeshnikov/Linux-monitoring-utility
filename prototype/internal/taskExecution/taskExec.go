f5b1c19b80d4dcb361cfea02001e0ca124cd01a6
mihyilo <90507951+mihyilo@users.noreply.github.com>
2024-03-20 17:00:20 +0300

    Update taskExec.go

--- prototype/internal/taskExecution/taskExec.go
+++ prototype/internal/taskExecution/taskExec.go
@@ -1,11 +1,13 @@
 package taskExecution
 
 import (
+	lsofLayer "linux-monitoring-utility/internal/lsofLayer"
+	rpmLayer "linux-monitoring-utility/internal/rpmLayer"
 	"sync"
 	"time"
 )
 
-func StartTasks(program_time int, bpftrace_time int, fileName string, toRun func(int, string)) {
+func StartTasks(program_time int, bpftrace_time int, fileName string, toRun func(int, string)) error {
 
 	var wg sync.WaitGroup
 
@@ -15,16 +17,30 @@ func StartTasks(program_time int, bpftrace_time int, fileName string, toRun func
 		defer wg.Done()
 		toRun(bpftrace_time, fileName)
 	}
+	wg.Add(1)
+	go bpftrace_run()
+
+	arr, err := lsofLayer.LsofExec()
+	if err != nil {
+		wg.Wait()
+		return err
+	}
+
+	err = rpmLayer.RPMlayer(arr)
+	if err != nil {
+		wg.Wait()
+		return err
+	}
 
 	for {
 		select {
 		case <-timer:
 			wg.Wait()
-			return
+			return nil
 		default:
 			wg.Add(1)
-			go bpftrace_run()
 			time.Sleep(time.Duration(bpftrace_time)*time.Second - 2*time.Second)
+			go bpftrace_run()
 		}
 	}
 
