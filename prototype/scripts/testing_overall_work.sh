003ed8727b54083d4e0a489ac9bd37cf7a0891a8
anutatesl <121693400+anutatesl@users.noreply.github.com>
2024-03-21 18:08:55 +0300

    Update testing_overall_work.sh

--- prototype/scripts/testing_overall_work.sh
+++ prototype/scripts/testing_overall_work.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-cd /home/$USER/Desktop
-mkdir checkPrototype
-cd checkPrototype
+sudo zypper install emacs
+sudo rpm -q emacs
+echo "install emacs"
 
-mkdir NewFolderNew
-cd NewFolderNew
-touch file.txt
-echo "create /Desktop/checkPrototype/NewFolderNew/file.txt"
 cd /
 
 echo "sleep 10m in"
@@ -15,30 +11,73 @@ echo "Run the prototype..."
 sleep 10m
 echo "sleep 10m out"
 
-for i in {1..10}; do
 
-    cd /home/$USER/Desktop/checkPrototype
-    directory="NewFolder$i"
-    mkdir $directory
-    cd $directory
+head -c0 /usr/bin/procinfo
+echo "open /usr/bin/procinfo from package procinfo-18-1.30.x86_64"
 
-    touch open_file_$i
-    cat /home/$USER/Desktop/checkPrototype/$directory/open_file_$i
-    echo "open /Desktop/checkPrototype/$directory/open_file_$i"
-    symlink="symlink$i"
-    ln -s open_file_$i $symlink
-    cat /home/$USER/Desktop/checkPrototype/$directory/symlink$i
-    echo "open /Desktop/checkPrototype/$directory/symlink$i"
-    touch not_open_file_$i
-    #cat /home/$USER/Desktop/checkPrototype/$directory/not_open_file_$i
-    echo "create /Desktop/checkPrototype/$directory/not_open_file_$i"
+echo "sleep 30m in"
+sleep 30m
+echo "sleep 30m out"
 
-    echo "sleep 30m in"
-    sleep 30m
-    echo "sleep 30m out"
-    #rm -R /home/$USER/Desktop/$directory
-    cd /
+head -c0 /usr/share/bash-completion/completions/tar
+echo "open /usr/share/bash-completion/completions/tar from package bash-completion-2.7-150400.13.3.1.noarch"
 
-done
+echo "sleep 30m in"
+sleep 30m
+echo "sleep 30m out"
 
-cd /
+head -c0 /usr/share/gcc-13/python/libstdcxx/v6/printers.py
+echo "open /usr/share/gcc-13/python/libstdcxx/v6/printers.py from package libstdc++6-pp-13.2.1+git7813-150000.1.6.1.x86_64"
+
+echo "sleep 30m in"
+sleep 30m
+echo "sleep 30m out"
+
+head -c0 /usr/share/locale/ar/LC_MESSAGES/glib-networking.mo
+echo "open /usr/share/locale/ar/LC_MESSAGES/glib-networking.mo from package glib-networking-lang-2.70.1-150400.1.6.noarch"
+
+echo "sleep 30m in"
+sleep 30m
+echo "sleep 30m out"
+
+head -c0 /usr/share/doc/packages/perl-Try-Tiny/README
+echo "open /usr/share/doc/packages/perl-Try-Tiny/README from package perl-Try-Tiny-0.30-1.17.noarch"
+
+echo "sleep 30m in"
+sleep 30m
+echo "sleep 30m out"
+
+head -c0 /usr/share/doc/packages/automake/AUTHORS
+echo "open /usr/share/doc/packages/automake/AUTHORS from package automake-1.15.1-4.10.2.noarch"
+
+echo "sleep 30m in"
+sleep 30m
+echo "sleep 30m out"
+
+head -c0 /usr/share/locale/wa/LC_MESSAGES/libkcompactdisc.mo
+echo "open /usr/share/locale/wa/LC_MESSAGES/libkcompactdisc.mo from package libkcompactdisc-lang-22.12.3-bp155.1.7.noarch"
+
+echo "sleep 30m in"
+sleep 30m
+echo "sleep 30m out"
+
+head -c0 /lib/firmware/amdgpu/verde_uvd.bin.xz
+echo "open /lib/firmware/amdgpu/verde_uvd.bin.xz from package kernel-firmware-amdgpu-20230724-150500.3.9.1.noarch"
+
+echo "sleep 30m in"
+sleep 30m
+echo "sleep 30m out"
+
+head -c0 /usr/share/doc/packages/patterns/base.txt
+echo "open /usr/share/doc/packages/patterns/base.txt from package patterns-base-base-20200505-lp155.10.5.x86_64"
+
+echo "sleep 30m in"
+sleep 30m
+echo "sleep 30m out"
+
+head -c0 /usr/lib64/liblzo2.so.2.0.0
+echo "open /usr/lib64/liblzo2.so.2.0.0 from package liblzo2-2-2.10-2.22.x86_64"
+
+echo "sleep 5m in"
+sleep 5m
+echo "sleep 5m out"
