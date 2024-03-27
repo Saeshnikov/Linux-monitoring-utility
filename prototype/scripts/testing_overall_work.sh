9228e1d9922a822a73ba17e7456b19b606c7f19f
anutatesl <121693400+anutatesl@users.noreply.github.com>
2024-03-27 21:05:26 +0300

    Update testing_overall_work.sh
    Аdded support for the file with the packages used in order to make the script "more active"

--- prototype/scripts/testing_overall_work.sh
+++ prototype/scripts/testing_overall_work.sh
@@ -2,82 +2,18 @@
 
 sudo zypper install emacs
 sudo rpm -q emacs
-echo "install emacs"
-
-cd /
+echo "install emacs emacs-27.2-150400.3.6.1.x86_64"
 
 echo "sleep 10m in"
 echo "Run the prototype..."
 sleep 10m
 echo "sleep 10m out"
 
+file="packagesUsed.txt"
 
-head -c0 /usr/bin/procinfo
-echo "open /usr/bin/procinfo from package procinfo-18-1.30.x86_64"
-
-echo "sleep 30m in"
-sleep 30m
-echo "sleep 30m out"
-
-head -c0 /usr/share/bash-completion/completions/tar
-echo "open /usr/share/bash-completion/completions/tar from package bash-completion-2.7-150400.13.3.1.noarch"
-
-echo "sleep 30m in"
-sleep 30m
-echo "sleep 30m out"
-
-head -c0 /usr/share/gcc-13/python/libstdcxx/v6/printers.py
-echo "open /usr/share/gcc-13/python/libstdcxx/v6/printers.py from package libstdc++6-pp-13.2.1+git7813-150000.1.6.1.x86_64"
-
-echo "sleep 30m in"
-sleep 30m
-echo "sleep 30m out"
-
-head -c0 /usr/share/locale/ar/LC_MESSAGES/glib-networking.mo
-echo "open /usr/share/locale/ar/LC_MESSAGES/glib-networking.mo from package glib-networking-lang-2.70.1-150400.1.6.noarch"
-
-echo "sleep 30m in"
-sleep 30m
-echo "sleep 30m out"
-
-head -c0 /usr/share/doc/packages/perl-Try-Tiny/README
-echo "open /usr/share/doc/packages/perl-Try-Tiny/README from package perl-Try-Tiny-0.30-1.17.noarch"
-
-echo "sleep 30m in"
-sleep 30m
-echo "sleep 30m out"
-
-head -c0 /usr/share/doc/packages/automake/AUTHORS
-echo "open /usr/share/doc/packages/automake/AUTHORS from package automake-1.15.1-4.10.2.noarch"
-
-echo "sleep 30m in"
-sleep 30m
-echo "sleep 30m out"
-
-head -c0 /usr/share/locale/wa/LC_MESSAGES/libkcompactdisc.mo
-echo "open /usr/share/locale/wa/LC_MESSAGES/libkcompactdisc.mo from package libkcompactdisc-lang-22.12.3-bp155.1.7.noarch"
-
-echo "sleep 30m in"
-sleep 30m
-echo "sleep 30m out"
-
-head -c0 /lib/firmware/amdgpu/verde_uvd.bin.xz
-echo "open /lib/firmware/amdgpu/verde_uvd.bin.xz from package kernel-firmware-amdgpu-20230724-150500.3.9.1.noarch"
-
-echo "sleep 30m in"
-sleep 30m
-echo "sleep 30m out"
-
-head -c0 /usr/share/doc/packages/patterns/base.txt
-echo "open /usr/share/doc/packages/patterns/base.txt from package patterns-base-base-20200505-lp155.10.5.x86_64"
-
-echo "sleep 30m in"
-sleep 30m
-echo "sleep 30m out"
-
-head -c0 /usr/lib64/liblzo2.so.2.0.0
-echo "open /usr/lib64/liblzo2.so.2.0.0 from package liblzo2-2-2.10-2.22.x86_64"
-
-echo "sleep 5m in"
-sleep 5m
-echo "sleep 5m out"
+while read -r line; do
+    package_file=$(sudo rpm -ql $line | head -n 1)
+    head -c0 $package_file
+    echo "open $package_file from package $line"
+    sleep 1
+done <$file
