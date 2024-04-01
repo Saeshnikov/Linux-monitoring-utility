66955fb25089c6f8bbb5927a07e11dab72d194ac
sonix14 <117933964+sonix14@users.noreply.github.com>
2024-04-01 19:20:35 +0300

    Update RPMLayer_test.go

--- prototype/internal/tests/RPMLayer_test.go
+++ prototype/internal/tests/RPMLayer_test.go
@@ -2,32 +2,65 @@ package tests
 
 import (
 	rpmLayer "linux-monitoring-utility/internal/rpmLayer"
+	"os"
 	"reflect"
 	"strconv"
 	"testing"
 )
 
 func TestFindUnusedPackages(t *testing.T) {
-	var allPackagesExample = make(map[string]bool)
-	for i := 1; i < 50; i++ {
-		allPackagesExample["package"+strconv.Itoa(i)] = true
+	allPackagesExample, usedPackagesExample, unusedPackagesExample := createTestArrays()
+	outputMap := allPackagesExample
+	type args struct {
+		allPackages  map[string]bool
+		usedPackages map[string]bool
+		dirPath      string
+		outputMap    *map[string]bool
 	}
+	tests := []struct {
+		name        string
+		args        args
+		expectedArr map[string]bool
+		wantErr     bool
+	}{
+		{"Basic test", args{allPackagesExample, usedPackagesExample, "", &outputMap}, unusedPackagesExample, false},
+		{"Negative test", args{allPackagesExample, usedPackagesExample, "", &outputMap}, usedPackagesExample, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := rpmLayer.FindUnusedPackages(tt.args.allPackages, tt.args.usedPackages, tt.args.dirPath, tt.args.outputMap)
+			eq := Equal(tt.expectedArr, outputMap)
+			if err != nil {
+				t.Errorf("FindUnusedPackages() error = %v", err)
+			} else if tt.wantErr {
+				if eq {
+					t.Error("Arrays with different data matched\n")
+				}
+			} else if !tt.wantErr {
+				if !eq {
+					t.Error("The resulting array did not match the expected one\n")
+				}
+			}
+		})
+	}
+	os.RemoveAll("./out/")
+}
+
+func createTestArrays() (map[string]bool, map[string]bool, map[string]bool) {
+	var allPackagesExample = make(map[string]bool)
 	var usedPackagesExample = make(map[string]bool)
-	for i := 1; i < 50; i++ {
+	var unusedPackagesExample = make(map[string]bool)
+	for i := 1; i <= 50; i++ {
+		allPackagesExample["package"+strconv.Itoa(i)] = true
 		if i%2 == 0 {
 			usedPackagesExample["package"+strconv.Itoa(i)] = true
+		} else {
+			unusedPackagesExample["package"+strconv.Itoa(i)] = true
 		}
 	}
+	return allPackagesExample, usedPackagesExample, unusedPackagesExample
+}
 
-	var outputMap = allPackagesExample
-	rpmLayer.FindUnusedPackages(allPackagesExample, usedPackagesExample, "", &outputMap)
-	for packageName := range usedPackagesExample {
-		if _, ok := allPackagesExample[packageName]; ok {
-			delete(allPackagesExample, packageName)
-		}
-	}
-
-	if !reflect.DeepEqual(allPackagesExample, outputMap) {
-		t.Error("The resulting array did not match the expected one")
-	}
+func Equal(unusedPackagesExp map[string]bool, unusedPackagesGot map[string]bool) bool {
+	return reflect.DeepEqual(unusedPackagesExp, unusedPackagesGot)
 }
