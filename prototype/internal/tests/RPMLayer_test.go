59074e08f3268e15408fed18e75e7afdd39f6686
sonix14 <117933964+sonix14@users.noreply.github.com>
2024-04-01 16:42:25 +0300

    Update RPMLayer_test.go

--- prototype/internal/tests/RPMLayer_test.go
+++ prototype/internal/tests/RPMLayer_test.go
@@ -1,57 +1,33 @@
 package tests
 
 import (
-	"bufio"
 	rpmLayer "linux-monitoring-utility/internal/rpmLayer"
-	"os"
-	"os/exec"
 	"reflect"
+	"strconv"
 	"testing"
 )
 
-func TestRPMLayer(t *testing.T) {
-	var allPackages = make(map[string]bool)
-	var outputMap = make(map[string]bool)
-	usedPackages, err := readFile("./usedPackages.txt")
-
-	cmd := exec.Command("/usr/bin/rpm", "-qa")
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		t.Fatal(err.Error())
+func TestFindUnusedPackages(t *testing.T) {
+	var allPackagesExample = make(map[string]bool)
+	for i := 1; i < 50; i++ {
+		allPackagesExample["package"+strconv.Itoa(i)] = true
 	}
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err.Error())
-	}
-	outScanner := bufio.NewScanner(stdout)
-	for outScanner.Scan() {
-		allPackages[outScanner.Text()] = true
-		outputMap[outScanner.Text()] = true
-	}
-
-	for _, packageName := range usedPackages {
-		if _, ok := allPackages[packageName]; ok {
-			delete(allPackages, packageName)
+	var usedPackagesExample = make(map[string]bool)
+	for i := 1; i < 50; i++ {
+		if i%2 == 0 {
+			usedPackagesExample["package"+strconv.Itoa(i)] = true
 		}
 	}
-	usedFiles, err := readFile("./usedFiles.txt")
-	rpmLayer.RPMlayer(usedFiles, "", outputMap)
-	eq := reflect.DeepEqual(allPackages, outputMap)
-	if eq {
-		t.Fatal("Test passed")
-	} else {
-		t.Fatal("Test failed")
-	}
-}
 
-func readFile(filePath string) ([]string, error) {
-	var arr []string
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
+	var outputMap = allPackagesExample
+	rpmLayer.FindUnusedPackages(allPackagesExample, usedPackagesExample, "", &outputMap)
+	for packageName := range usedPackagesExample {
+		if _, ok := allPackagesExample[packageName]; ok {
+			delete(allPackagesExample, packageName)
+		}
 	}
-	fileScanner := bufio.NewScanner(file)
-	for fileScanner.Scan() {
-		arr = append(arr, fileScanner.Text())
+
+	if !reflect.DeepEqual(allPackagesExample, outputMap) {
+		t.Error("The resulting array did not match the expected one")
 	}
-	return arr, nil
 }
