6c56b0fe3d142b4cfca04039f3b882be6c3b7de5
Saeshnikov <103879754+Saeshnikov@users.noreply.github.com>
2024-04-04 14:06:33 +0300

    Create lsofExec_test.go

---
+++ prototype/internal/tests/lsofExec_test/lsofExec_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"linux-monitoring-utility/internal/lsofLayer"
+	"os"
+	"testing"
+)
+
+func TestLsofExec(t *testing.T) {
+	file, err := os.Create("/usr/tmp")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer os.Remove(file.Name())
+	arr_res, err := lsofLayer.LsofExec()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	for _, s := range arr_res {
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+		if s == file.Name() {
+			return
+		}
+	}
+	t.Fatal("Test failed")
+}
