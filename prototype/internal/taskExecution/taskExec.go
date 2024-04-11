79fbdb30ad77adc5ae6ec4903a22e8385ee20626
mihyilo <90507951+mihyilo@users.noreply.github.com>
2024-04-11 16:09:12 +0300

    Update taskExec.go

--- prototype/internal/taskExecution/taskExec.go
+++ prototype/internal/taskExecution/taskExec.go
@@ -16,6 +16,7 @@ func StartTasks(program_time uint, bpftrace_time uint, fileName string, outputPa
 	timer := time.After(time.Duration(program_time) * time.Second)
 	c := make(chan *os.Process, 1)
 	errorChan := make(chan error, 1)
+	var curProc *os.Process = nil
 	var prevProc *os.Process = nil
 	lsof_run := func() {
 		fmt.Printf("Lsof started...\n")
@@ -39,17 +40,20 @@ func StartTasks(program_time uint, bpftrace_time uint, fileName string, outputPa
 		toRun(bpftrace_time, fileName, outputPath, outputMap, c)
 	}
 	flag := false
+
 	for {
 		select {
 		case err := <-errorChan:
 			return err
 		case <-timer:
+			curProc.Signal(os.Interrupt)
+			fmt.Printf("Stopping previous process with PID: %d\n", curProc.Pid)
 			wg.Wait()
 			return nil
 		default:
 			wg.Add(1)
 			go bpftrace_run()
-			curProc := <-c
+			curProc = <-c
 			if !flag {
 				go lsof_run()
 				flag = true
@@ -66,3 +70,4 @@ func StartTasks(program_time uint, bpftrace_time uint, fileName string, outputPa
 	}
 
 }
+
