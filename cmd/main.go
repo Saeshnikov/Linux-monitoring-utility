ab29fd447124831187ba65c9e8b0e457bb9f789f
Egor <saeshnikoff@yandex.com>
2024-05-17 18:49:17 +0300

    taskExec reworked

--- cmd/main.go
+++ cmd/main.go
@@ -12,9 +12,8 @@ import (
 	taskExecution "linux-monitoring-utility/internal/taskExecution"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"strconv"
+	"strings"
 	"time"
 )
 
@@ -61,10 +60,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = taskExecution.StartTasks(programConfig.ProgramTime, programConfig.ScriptTime, bpfScriptFile.Name(), toRun, toRunLsof)
-	if err != nil {
-		log.Fatal(err)
-	}
+	lsof := taskExecution.ExecUnit{BinPath: "/usr/bin/lsof", ExecCount: 1}
+	bpf := taskExecution.ExecUnit{BinPath: programConfig.BpftraceBinPath, Args: bpfScriptFile.Name(), ExecCount: uint(programConfig.ProgramTime / programConfig.ScriptTime), IsContinuous: true, ExecTime: time.Duration(programConfig.ScriptTime) * time.Second}
+	taskExecConf := []taskExecution.ExecUnit{lsof, bpf}
+	taskExecution.StartTasks(taskExecConf, pathToTmp)
 
 	err = toAnalyse(pathToTmp+"/tmp/", programConfig.OutputPath, &outputMap)
 	if err != nil {
@@ -75,52 +74,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func toRunLsof() {
-	arr, err := lsofLayer.LsofExec()
-	if err != nil {
-		log.Fatal(err)
-	}
-	file, err := os.Create(pathToTmp + "/tmp/" + "lsofOutput")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, line := range arr {
-		file.WriteString(line + "\n")
-	}
-}
-
-func toRun(fileName string, c chan *exec.Cmd) {
-
-	file, err := os.Create(pathToTmp + "/tmp/" + strconv.FormatInt(time.Now().Unix(), 10))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	cmd := exec.Command(programConfig.BpftraceBinPath, fileName)
-	pipe, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	reader := bufio.NewReader(pipe)
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Procces is running as pid %d\n", cmd.Process.Pid)
-	line, err := reader.ReadString('\n')
-
-	c <- cmd
-
-	for err == nil {
-		file.WriteString(line)
-		line, err = reader.ReadString('\n')
-	}
-
-	cmd.Wait()
 }
 
 func exportToJson(filePath string, outputMap map[string]bool) error {
@@ -152,7 +105,7 @@ func toAnalyse(directory string, dirPath string, outputMap *map[string]bool) err
 	}
 	for _, file := range files {
 		if !file.IsDir() {
-			if file.Name() == "lsofOutput" {
+			if strings.Split(file.Name(), ".")[0] == "lsof" {
 				var res []string
 				filePath := filepath.Join(directory, file.Name())
 				f, err := os.Open(filePath)
@@ -161,10 +114,8 @@ func toAnalyse(directory string, dirPath string, outputMap *map[string]bool) err
 				}
 				defer f.Close()
 				scanner := bufio.NewScanner(f)
-				for scanner.Scan() {
-					line := scanner.Text()
-					res = append(res, line)
-				}
+				res, err = lsofLayer.LsofParsing(scanner)
+
 				fmt.Print("File with name: ", file.Name(), " to analyse... ")
 				rpmLayer.RPMlayer(res, dirPath, outputMap)
 				fmt.Print(" DONE\n")
--- internal/taskExecution/taskExec.go
+++ internal/taskExecution/taskExec.go
@@ -1,61 +1,175 @@
 package taskExecution
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
 
-func StartTasks(program_time uint, bpftrace_time uint, fileName string, toRun func(string, chan *exec.Cmd), toRunLsof func()) error {
+type ExecUnit struct {
+	BinPath      string
+	Args         string
+	ExecCount    uint
+	IsContinuous bool
+	ExecTime     time.Duration
+}
+
+var processes []*exec.Cmd
+
+func StartTasks(toExec []ExecUnit, outDirPath string) {
 	var wg sync.WaitGroup
-
-	timer := time.After(time.Duration(program_time) * time.Second)
-	c := make(chan *exec.Cmd, 1)
-	var curProc *exec.Cmd = nil
-	var prevProc *exec.Cmd = nil
-	lsof_run := func() {
+	processes = make([]*exec.Cmd, len(toExec))
+	outToFile := func(filename string, c <-chan bytes.Buffer) {
 		defer wg.Done()
-		fmt.Printf("Lsof started...\n")
-		toRunLsof()
+		b, ok := <-c
+		if !ok {
+			return
+		}
+		tmp := strings.Split(filename, "/")
+
+		filename = tmp[len(tmp)-1]
+
+		file, err := os.Create(outDirPath + "/tmp/" + filename + "." + strconv.FormatInt(time.Now().Unix(), 10))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		file.Write(b.Bytes())
 	}
 
-	bpftrace_run := func() {
+	execOneShot := func(binPath string, args string, execCount uint) {
 		defer wg.Done()
-		toRun(fileName, c)
-	}
-	flag := false
-
-	for {
-		select {
-		case <-timer:
-			err := curProc.Process.Signal(os.Interrupt)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("Stopping previous process with PID: %d\n", curProc.Process.Pid)
-			wg.Wait()
-			return nil
-		default:
+		for execCount_ := 0; execCount_ < int(execCount); execCount_++ {
+			buf := make(chan bytes.Buffer, 1)
+			fmt.Printf("%s Started...\n", binPath)
+			toRunOneShot(binPath, args, buf)
 			wg.Add(1)
-			go bpftrace_run()
-			curProc = <-c
-			if !flag {
-				wg.Add(1)
-				go lsof_run()
-				flag = true
-			}
+			go outToFile(binPath, buf)
+		}
+	}
+
+	execContinuous := func(index int, binPath string, args string, execCount uint, execTime time.Duration) {
+		defer wg.Done()
+		con_run := func(p chan *exec.Cmd, c chan bytes.Buffer) {
+			defer wg.Done()
+			toRunContinuous(binPath, args, p, c)
+
+		}
+		var prevProc *exec.Cmd
+		p := make(chan *exec.Cmd, 1)
+		for execCount_ := 0; execCount_ < int(execCount); execCount_++ {
+			buf := make(chan bytes.Buffer, 1)
+			wg.Add(1)
+			go con_run(p, buf)
+			fmt.Printf("%s Started...\n", binPath)
+			processes[index] = <-p
 			if prevProc != nil {
 				err := prevProc.Process.Signal(os.Interrupt)
 				if err != nil {
-					return err
+					intAllProcesses(err)
 				}
-				fmt.Printf("Stopping previous process with PID: %d\n", prevProc.Process.Pid)
+				fmt.Printf("Stopping %s process with PID: %d\n", binPath, prevProc.Process.Pid)
 
 			}
-			prevProc = curProc
-			time.Sleep(time.Duration(bpftrace_time) * time.Second)
+			prevProc = processes[index]
+			wg.Add(1)
+			go outToFile(binPath, buf)
+			time.Sleep(execTime)
+		}
+
+		err := prevProc.Process.Signal(os.Interrupt)
+		if err != nil {
+			intAllProcesses(err)
+		}
+		fmt.Printf("Stopping %s process with PID: %d\n", binPath, prevProc.Process.Pid)
+	}
+
+	for index, unit := range toExec {
+
+		if !unit.IsContinuous {
+			wg.Add(1)
+			go execOneShot(unit.BinPath, unit.Args, unit.ExecCount)
+		} else {
+			wg.Add(1)
+			go execContinuous(index, unit.BinPath, unit.Args, unit.ExecCount, unit.ExecTime)
 		}
 	}
+
+	wg.Wait()
+
+}
+
+// poka skip
+func intAllProcesses(err error) {
+	log.Fatal(err)
+}
+
+func toRunOneShot(binPath string, args string, c chan<- bytes.Buffer) {
+	var cmd *exec.Cmd
+	if args != "" {
+		cmd = exec.Command(binPath, args)
+	} else {
+		cmd = exec.Command(binPath)
+	}
+
+	pipe, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(pipe)
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	var buffer bytes.Buffer
+
+	line, err := reader.ReadString('\n')
+
+	for err == nil {
+		buffer.WriteString(line)
+		line, err = reader.ReadString('\n')
+	}
+	cmd.Wait()
+	c <- buffer
+}
+
+func toRunContinuous(binPath string, args string, p chan<- *exec.Cmd, c chan<- bytes.Buffer) {
+
+	var buffer bytes.Buffer
+
+	var cmd *exec.Cmd
+	if args != "" {
+		cmd = exec.Command(binPath, args)
+	} else {
+		cmd = exec.Command(binPath)
+	}
+	pipe, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(pipe)
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Procces is running as pid %d\n", cmd.Process.Pid)
+	line, err := reader.ReadString('\n')
+
+	p <- cmd
+
+	for err == nil {
+		buffer.WriteString(line)
+		line, err = reader.ReadString('\n')
+	}
+
+	cmd.Wait()
+	c <- buffer
 }
