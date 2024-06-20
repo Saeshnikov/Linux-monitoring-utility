ffc6270b0830d495a8ead4c79c028a73e0dc2d11
anutatesl <121693400+anutatesl@users.noreply.github.com>
2024-06-20 12:42:45 +0300

    Added sharedMem script and posixSem test

---
+++ scripts/shm_test/posix/semaphore-posix.c
@@ -0,0 +1,35 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include <stdio.h>
+
+#define SEMAPHORE_NAME "/my_named_semaphore"
+
+int main(int argc, char ** argv) {
+    sem_t *sem;
+
+    if ( argc == 2 ) {
+        printf("Dropping semaphore...\n");
+        if ( (sem = sem_open(SEMAPHORE_NAME, 0)) == SEM_FAILED ) {
+            perror("sem_open");
+            return 1;
+        }
+        sem_post(sem);
+        perror("sem_post");
+        printf("Semaphore dropped.\n");
+        return 0;
+    }
+
+    if ( (sem = sem_open(SEMAPHORE_NAME, O_CREAT, 0777, 0)) == SEM_FAILED ) {
+        perror("sem_open");
+        return 1;
+    }
+
+    printf("Semaphore is taken.\nWaiting for it to be dropped.\n");
+    if (sem_wait(sem) < 0 )
+        perror("sem_wait");
+    if ( sem_close(sem) < 0 )
+        perror("sem_close");
+
+    return 0;
+}
