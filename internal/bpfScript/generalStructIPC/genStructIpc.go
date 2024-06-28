e2f91c42a76ef985a9b43035791c924b9dc5a1c9
anutatesl <121693400+anutatesl@users.noreply.github.com>
2024-06-28 08:24:53 +0300

    Update genStructIpc.go for yaml format

--- internal/bpfScript/generalStructIPC/genStructIpc.go
+++ internal/bpfScript/generalStructIPC/genStructIpc.go
@@ -1,11 +1,11 @@
 package generalStructIPC
 
 type OptionStruct struct {
-	OptionType string
-	Options    []string
+	OptionType string `yaml:"optionType"`
+	Options    []string `yaml:"options"`
 }
 
 type IpcStruct struct {
-	IpcType string
-	Option  []OptionStruct
+	IpcType string `yaml:"ipcType"`
+	Option  []OptionStruct `yaml:"option"`
 }
