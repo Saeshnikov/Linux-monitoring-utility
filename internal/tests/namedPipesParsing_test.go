a9e59a20f1f43ee5bb7126702fd638d0d675ff67
sonix14 <117933964+sonix14@users.noreply.github.com>
2024-07-26 00:23:23 +0300

    fix file name

--- internal/tests/namedPipesParsing_test.go
+++ internal/tests/namedPipesParsing_test.go
@@ -21,7 +21,7 @@ func TestParsePipes(t *testing.T) {
 		args        args
 		expectedArr []parsingstruct.ParsingData
 	}{
-		{"Basic test", args{"./data/testNamedPipes.txt"}, ParsedDataExample},
+		{"Basic test", args{"./data/namedPipesParsingTest.txt"}, ParsedDataExample},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
