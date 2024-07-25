07e78ebad73930ec4d9f11bf5c07ff838d951c72
sonix14 <117933964+sonix14@users.noreply.github.com>
2024-07-26 00:22:20 +0300

    fix file name

--- internal/tests/semaphoreParsing_test.go
+++ internal/tests/semaphoreParsing_test.go
@@ -25,7 +25,7 @@ func TestParseSemaphores(t *testing.T) {
 		args        args
 		expectedArr []parsingstruct.ParsingData
 	}{
-		{"Basic test", args{"./data/testSemaphores.txt"}, ParsedDataExample},
+		{"Basic test", args{"./data/semaphoreParsingTest.txt"}, ParsedDataExample},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
